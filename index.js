30ee696d35a5f5f89e1a4db1ec4968feaaa9dbd0
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-27 00:53:04 +0200

    setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+// // User
+// const usersSchema = new Schema({  message: String});
+// const User = mongoose.model("User", usersSchema);
+
+// // comments
+// const commentsSchema = new Schema({  
+//   message: String,
+//   // user:usersSchema
+//   });
+const Comment = mongoose.model("Comment", commentsSchema);
+""
+mongoose
+  .connect("mongodb://localhost:27017/relations2", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(()=>{})
+  .catch((err)=>{
+    console.log("erorrrr")
+    console.log(err)
+  })
+  .finally(() => mongoose.connection.close())
+
+// async function main() {
+//   const user= await new User({ name: "Ajil"}).save();
+//   await new Comment({message: "Hi", user:user}).save();
+
+// }
